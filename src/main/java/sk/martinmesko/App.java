22a6dd8dcb9165ec4b076ce52f4421bee8e08e2e
MartinMesko <alegremato@gmail.com>
2024-03-02 20:43:59 +0100

    .

--- src/main/java/sk/martinmesko/App.java
+++ src/main/java/sk/martinmesko/App.java
@@ -6,18 +6,18 @@ public class App {
     final static char PRAZDNO = ' ';
     final static char X = 'X';
     final static char O = 'O';
-    static char hraTeraz = X;
+    static char hraTerazHrac = X;
     static char[][] board = {{PRAZDNO, PRAZDNO, PRAZDNO}, {PRAZDNO, PRAZDNO, PRAZDNO}, {PRAZDNO, PRAZDNO, PRAZDNO}};
 
     public static void main(String[] args) {
-        zobrazenieHracejPlochy();
-        int input = vstupOdHraca();
-        aktualizaciaHry(input);
-        System.out.println(input);
-        System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+        while (!koniecHry()) {
+            zobrazenieHracejPlochy();
+            int input = vstupOdHraca();
+            aktualizaciaHry(input);
+        }
     }
 
-    static void zobrazenieHracejPlochy() {
+    public static void zobrazenieHracejPlochy() {
         for (char[] riadok : board) {
             for (int i = 0; i < riadok.length / 3; i++) {
                 System.out.println(riadok[0] + " | " + riadok[1] + " | " + riadok[2]);
@@ -27,9 +27,9 @@ public class App {
     }
 
     //ziskanie vstupu od hraca
-    static int vstupOdHraca() {
+    public static int vstupOdHraca() {
         int input = 0;
-        System.out.println("Zadaj číslo medzi 1 až 9 \n");
+        System.out.println("Hráč " + hraTerazHrac + " zadaj číslo medzi 1 až 9 \n");
         while (true) {
 
             Scanner scanner = new Scanner(System.in);
@@ -50,16 +50,42 @@ public class App {
     }
 
     //aktualizacia hry
-    static void aktualizaciaHry(int input) {
-
-        board[(input - 1) / 3][(input - 1) % 3] = hraTeraz;
-        hraTeraz = (hraTeraz == X) ? O : X;
-        zobrazenieHracejPlochy();
-
+    public static void aktualizaciaHry(int input) {
+            board[(input-1) / 3][(input-1) % 3] = hraTerazHrac;
+            hraTerazHrac = (hraTerazHrac == X) ? O : X;
     }
 
+    //kontrola riadkov
+    public static boolean riadky() {
+        for(char[]riadky : board)
+        {
+            if (riadky[0] == hraTerazHrac && riadky[1] == hraTerazHrac && riadky[2] == hraTerazHrac)
+            {
+
+                System.out.println("Vyhral hráč " + hraTerazHrac);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //kontrola stĺpcov
+    public static boolean stlpce() {
+        return false;
+    }
+
+    //kontrola diagonal
+    public static boolean diagonaly() {
+
+        return false;
+    }
+
+    //koniecHry
+    public static boolean koniecHry() {
+        if (riadky() || stlpce() || diagonaly()) {
+            return true;
+        }
+        return false;
+    }
 }
-
-
-
-
